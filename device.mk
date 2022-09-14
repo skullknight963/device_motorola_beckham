da53173d545dedfaa468e79761e22615ce90c8c1
dianlujitao <dianlujitao@lineageos.org>
2022-09-14 19:11:32 +0100

    beckham: Build libsensorndkbridge from source
    Change-Id: I9b00ac56b98314350c36523e02b5898193d89c0b

--- device.mk
+++ device.mk
@@ -415,7 +415,8 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf \
